422e7cc84b688e61f4e33eb02d4de7283a3e01f6
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 16:47:43 +0530

    Add ItemCount - Incremenet / decrement

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -2,6 +2,10 @@ import './ItemCount.css'
 
 export default function ItemCount() {
   return (
-    <div>ItemCount</div>
+    <div className='item-count__container'>
+      <button className='item-count__decrement'>-</button>
+      <input className='item-count__input' type="number" />
+      <button className='item-count__increment'>+</button>
+    </div>
   )
 }
