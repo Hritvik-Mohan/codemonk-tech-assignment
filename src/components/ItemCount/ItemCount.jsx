792fb745477cd06eba2e149896a89cb9543fd625
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 19:58:04 +0530

    Add quantity counter functionality

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react'
 import './ItemCount.css'
 
 export default function ItemCount() {
+
+  const [counter, setCounter] = useState(0);
+
+  function decrement () {
+    if (counter > 0) {
+      setCounter(count => count - 1);
+    }
+  }
+
+  function increment() {
+    setCounter(count => count + 1);
+  }
+
+  function handleChange() {
+    // console.log("handleChange")
+  }
+
   return (
     <div className='item-count__container'>
-      <button className='item-count__decrement'>-</button>
-      <input className='item-count__input' type="number" />
-      <button className='item-count__increment'>+</button>
+      <button className='item-count__decrement' onClick={decrement}>-</button>
+      <input
+        type="number"
+        name="quantity"
+        value={counter}
+        onChange={handleChange}
+        className='item-count__input' 
+      />
+      <button className='item-count__increment' onClick={increment}>+</button>
     </div>
   )
 }
