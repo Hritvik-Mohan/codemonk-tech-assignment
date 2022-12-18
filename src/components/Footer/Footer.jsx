168eee9724c545926d302b5a67d3673302d6a2bc
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 18:01:18 +0530

    Add Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import './Footer.css'
+import '../HeaderBar/HeaderBar.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className="container">
+            <div className="footer-mob">
+                <div className="footer-mob__links-section-1">
+                    <span>Dummy</span>
+                    <span className='links-divider'></span>
+                    <span>Dummy</span>
+                    <span className='links-divider'></span>
+                    <span>Dummy</span>
+                </div>
+                <div className="footer-mob__links-section-2">
+                    Lorem Ipsum is simply dummy text inting.
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
