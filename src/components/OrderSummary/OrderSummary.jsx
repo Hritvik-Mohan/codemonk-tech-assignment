d0559fd4506c4c666b89a109cea464f24f813ae0
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 16:49:31 +0530

    Add Order Summary

---
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,14 @@
+import './OrderSummary.css'
+import '../DeliveryAvailability/DeliveryAvailability.css'
+
+export default function OrderSummary() {
+  return (
+    <section className='delivery-availability'>
+    <div className="delivery-availability__container container">
+
+        <h3>Order Summary (xx items)</h3>
+
+    </div>
+</section>
+  )
+}
