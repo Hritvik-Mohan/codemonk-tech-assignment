e4bf04762fbf2a2c41eed86aab3fe84207ecc01c
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 16:49:16 +0530

    Add Delivery Availability

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 import Banner from './components/Banner/Banner'
 import Cart from './components/Cart/Cart'
 import CartNav from './components/CartNav/CartNav'
+import DeliveryAvailability from './components/DeliveryAvailability/DeliveryAvailability'
 import HeaderBar from './components/HeaderBar/HeaderBar'
+import OrderSummary from './components/OrderSummary/OrderSummary'
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
       <CartNav />
       <Banner />
       <Cart />
+      <DeliveryAvailability />
+      <OrderSummary />
     </div>
   )
 }
---
+++ src/components/DeliveryAvailability/DeliveryAvailability.jsx
@@ -0,0 +1,60 @@
+import './DeliveryAvailability.css';
+import location from '../../assets/icon/LOCATION.png'
+import ButtonType2 from '../Button/ButtonType2/ButtonType2';
+import check from '../../assets/icon/check.png'
+
+export default function DeliveryAvailability() {
+  return (
+    <section className='delivery-availability'>
+        <div className="delivery-availability__container container">
+
+            <h3>Delivery Availability</h3>
+
+            <div className="delivery-availability__input__container">
+                <div>
+                    <div className="delivery-availability__location-icon">
+                        <img src={location} alt="" />
+                    </div>
+                    <div className="delivery-availability__input">
+                        <input type="number" placeholder='PINCODE' />
+                    </div>
+                </div>
+                <div className='delivery-availability__change-button-container'>
+                    <ButtonType2 text="Change"/>
+                </div>
+            </div>
+
+            <div className="delivery-availability__check-container">
+
+                <div className="delivery-availability__check">
+                    <div className="check">
+                        <img src={check} alt="" />
+                    </div>
+                    <div className="delivery-availability__check-text">
+                        Free delivery
+                    </div>
+                </div>
+
+                <div className="delivery-availability__check">
+                    <div className="check">
+                        <img src={check} alt="" />
+                    </div>
+                    <div className="delivery-availability__check-text">
+                        Cash on delivery
+                    </div>
+                </div>
+
+                <div className="delivery-availability__check">
+                    <div className="check">
+                        <img src={check} alt="" />
+                    </div>
+                    <div className="delivery-availability__check-text">
+                        Estimated delivery time iss 3-5 days
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
