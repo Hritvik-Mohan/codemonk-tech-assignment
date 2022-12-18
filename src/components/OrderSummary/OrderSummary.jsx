bfd68527b2a0b41deb042664797904e0cf79e8fa
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-18 18:02:23 +0530

    Update Order Summary

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -8,6 +8,28 @@ export default function OrderSummary() {
 
         <h3>Order Summary (xx items)</h3>
 
+        <div className="order-summary">
+            <div className="order-summary__section order-summary__subtotal">
+                <p>Subtotal</p>
+                <p>2500 <span>$</span></p>
+            </div>
+
+            <div className="order-summary__section order-summary__total-discount">
+                <p>Total Discount</p>
+                <p>-100 <span>$</span></p>
+            </div>
+
+            <div className="order-summary__section order-summary__standard-shipping">
+                <p>Standart Shipping</p>
+                <p>Free</p>
+            </div>
+        </div>
+
+        <div className="order-summary__grand-total">
+            <p>Grand Total</p>
+            <p>42 998,00 <span>$</span></p>
+        </div>
+
     </div>
 </section>
   )
