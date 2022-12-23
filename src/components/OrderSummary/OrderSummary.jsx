40f8029f1b1a0cea96aede58b3a468d6a09d457f
Hritvik Mohan <hritvik5005@gmail.com>
2022-12-23 22:16:49 +0530

    Add: Order Summary Functionality

--- src/components/OrderSummary/OrderSummary.jsx
+++ src/components/OrderSummary/OrderSummary.jsx
@@ -1,13 +1,32 @@
 import './OrderSummary.css'
 import '../DeliveryAvailability/DeliveryAvailability.css'
 import data from '../../data.json'
+import { CartState } from '../../context/Context';
+import { useEffect, useState } from 'react';
 
 export default function OrderSummary(props) {
+
+    const {
+        state: { cartProducts, total },
+        dispatch,
+    } = CartState();
+
+    // console.log(total.total)
+
+    // const [subtotal, setSubtotal] = useState(total.total);
+    // useEffect(() => {
+    //     setSubtotal(total.total)
+    // },[total.total])
+    // console.log(subtotal)
+
+    console.log(props)
+
+    console.log(data.discount)
 
     // console.log(data.discount)
 
     // console.log(props.products.price)
-    console.log(props.isAva)
+    // console.log(props.isAva)
 
   return (
     <section className='delivery-availability'>
@@ -18,12 +37,12 @@ export default function OrderSummary(props) {
         <div className="order-summary">
             <div className="order-summary__section order-summary__subtotal">
                 <p>Subtotal</p>
-                <p>{props.products.price} <span>$</span></p>
+                <p>{props.products.length ? total.total : 0} <span>$</span></p>
             </div>
 
             <div className="order-summary__section order-summary__total-discount">
                 <p>Total Discount</p>
-                <p><span>$</span></p>
+                <p><span>-{props.products.length ? total.total * 0.1 : 0} $</span></p>
             </div>
 
             <div className="order-summary__section order-summary__standard-shipping">
@@ -36,7 +55,7 @@ export default function OrderSummary(props) {
     <div className="order-summary__grand-total">
         <p className='order-summary__grand-total-text'>Grand Total</p>
         <p className='order-total'>Order Total</p>
-        <p className='order-summary__grand-total-price'>0 <span>$</span></p>
+        <p className='order-summary__grand-total-price'>{total.total - total.total * 0.1 } <span>$</span></p>
     </div>
 </section>
   )
